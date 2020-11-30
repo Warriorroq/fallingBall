800cc1fd0d5f8af84466ecefc6fdf62db70467bf
Warriorroq <Warriorroq@gmail.com>
2020-11-30 12:29:30 +0200

    Added multy-ball

--- balltest/balltest/balltest.cpp
+++ balltest/balltest/balltest.cpp
@@ -2,10 +2,15 @@
 #include "Math.h"
 #include <string>
 #include <SFML/Graphics.hpp>
+#include <cstdlib>
 
 using namespace sf;
 
 using namespace std;
+
+
+const float gravity = 9.8f;
+const float FPS = 75.0f;
 
 namespace myVector {
 	struct Vector3 {
@@ -79,25 +84,58 @@ namespace myVector {
 		}
 	};
 }
+struct Ball {
+public:
+	myVector::Vector3 velocity;
+	myVector::Vector3 position;
+	myVector::Vector3 color;
+	CircleShape circle;
+	Ball(myVector::Vector3 position = myVector::Vector3(0, 0), double raduis = 10, myVector::Vector3 velocity = myVector::Vector3(0, 0), myVector::Vector3 color = myVector::Vector3(255, 255, 255)) : position(position), velocity(velocity),color(color){
+		circle = CircleShape(raduis);
+		circle.setPosition(this->position.x, this->position.y);
+		circle.setFillColor(Color(this->color.x, this->color.y, this->color.z));
+		velocity2 = this->velocity;
+	}
+	void Move(float frameTime) {
 
-int main()
-{
-	RenderWindow window(VideoMode(800, 400, 32), "Hello");
-	Clock clock;
-	Event ev;
-	const float FPS = 75.0f;
-	float frameTime = 1.0f / FPS;
-	bool draw = true;
-	float gravity = 9.8f;
-	//////////////////////////////////////
-	myVector::Vector3 pos(0, 0);
-	myVector::Vector3 velocity(0, 0);
-	myVector::Vector3 radius(10);
-	myVector::Vector3 color(255, 255, 255);
+		if (position.y < 390)
+			velocity.y += gravity * frameTime;
 
+		if (position.y > 400)
+			velocity.y *= -0.9f;
+
+		if (position.x < -5 || position.x > 790)
+			velocity.x *= -1;
+
+		if (velocity.x > 0)
+			velocity.x -= velocity2.x / 10.0f * frameTime;
+		else
+			velocity.x += velocity2.x / 10.0f * frameTime;
+
+		position += velocity;
+
+		circle.setPosition(this->position.x, this->position.y);
+	}
+private:
+	myVector::Vector3 velocity2;
+};
+
+Ball CreateBall() {
 	float power = -1;
 	float angle = 100;
-	float startPos = -1;		
+	float startPos = -1;
+
+	myVector::Vector3 pos(0, 0);
+	myVector::Vector3 velocity(0, 0);
+	float radius = 10;
+	myVector::Vector3 color(255, 255, 255);
+
+
+	while (!(startPos >= 0 && startPos <= 380)) {
+		cout << "start Y: (>0 & <380)";
+		cin >> startPos;
+		pos.y = 380 - startPos;
+	}
 
 	while (power < 0) {
 		cout << "power: (>0)";
@@ -110,53 +148,80 @@ int main()
 
 		velocity.x = power * sinf(angle);
 
-		if(angle < 45)
+		if (angle < 45)
 			velocity.y = -power * cosf(angle);
-		else 
+		else
 			velocity.y = power * cosf(angle);
 	}
+	return Ball(pos, radius, velocity, color);
+}
+Ball CreateRandomBall() {
+	float power = -1;
+	float angle = 100;
+	float startPos = -1;
 
+	myVector::Vector3 pos(0, 0);
+	myVector::Vector3 velocity(0, 0);
+	float radius = 10;
+	myVector::Vector3 color(255, 255, 255);
 
-	while (!(startPos >= 0 && startPos <= 380)) {
-		cout << "start Y: (>0 & <380)";
-		cin >> startPos;
-		pos.y = 380 - startPos;
+	pos.y = 380 -abs( rand() % 360 + 20);
+
+	power = abs( rand() % 10) + 5;
+	color.x = abs(rand() % 255);
+	color.y = abs(rand() % 255);
+	color.z = abs(rand() % 255);
+
+	angle = abs( rand() % 90) + 15;
+
+	velocity.x = power * sinf(angle);
+
+	if (angle < 45)
+		velocity.y = power * cosf(angle);
+	else
+		velocity.y = -power * cosf(angle);
+
+	return Ball(pos, radius, velocity, color);
+}
+
+void AddBall(Ball &bal,Ball *bals,int &thisbal) {
+	bals[thisbal] = bal;
+	thisbal++;
+}
+int main()
+{
+	RenderWindow window(VideoMode(800, 400, 32), "Hello");
+	Clock clock;
+	Event ev;
+	float frameTime = 1.0f / FPS;
+	bool draw = true;
+
+	//////////////////////////////////////
+	int balls = 0;
+	cout << "how many balls you want (1<x<11)" << endl;
+	cin >> balls;
+	balls = abs(balls);
+	Ball bal[12];
+	cout << "coms : 1 - random | 0 - my ball";
+	for(int i =0; i< balls;i++){
+		int a = 0;
+		cin >> a;
+		Ball baL;
+		if (a == 0)
+			baL = CreateBall();
+		else
+			baL = CreateRandomBall();
+		bal[i] = baL;
 	}
-
-	myVector::Vector3 pos2 = pos;
-	velocity.x = abs(velocity.x);
-	myVector::Vector3 velocity2 = velocity;
-
-
-	velocity.PrintInfo();	
-
-	CircleShape circle(radius.x);
-	circle.setPosition(pos.x, pos.y);
-	circle.setFillColor(Color(color.x, color.y, color.z));
-	///////////////////////////////////////////////////
+	//////////////////////////////////////
 
 	while (window.isOpen())
 	{
 		if (clock.getElapsedTime().asSeconds() >= frameTime) {
 			draw = true;
-			pos += velocity;
-
-			if (pos.y < 390)
-					velocity.y += gravity * frameTime;
-
-			circle.setPosition(pos.x, pos.y);
 			clock.restart();
- 
-			if (pos.y > 390) 
-				velocity.y *= -0.9f;
-
-			if (pos.x < -5 || pos.x > 790)
-				velocity.x *= -1;
-
-			if(velocity.x > 0)
-				velocity.x -= velocity2.x / 10.0f * frameTime;
-			else 
-				velocity.x += velocity2.x / 10.0f * frameTime;
+			for (int i = 0; i < balls; i++) 
+				bal[i].Move(frameTime);
 		}
 		else {
 			Time sleepTime = seconds((frameTime)-clock.getElapsedTime().asSeconds());
@@ -170,7 +235,10 @@ int main()
 		if (draw) {
 			draw = false;
 			window.clear(Color::Black);
-			window.draw(circle);
+			for (int i = 0; i < balls; i++) {
+				window.draw(bal[i].circle);
+			}
+
 			window.display();
 		}
 	}
